8593f9a3d877b8f46504be65b5fcf81cbf888bbc
Peter Bay Bastian <peter.bastian@gmail.com>
2014-02-02 18:54:24 +0100

    Removed build output.

--- baelish-app/build/app/routes.js
+++
@@ -1,9 +0,0 @@
-//# sourceMappingURL=routes.map
-require('source-map-support').install();
-(function() {
-  module.exports = {
-    "/": require("./pages/home"),
-    "/about/:name": require("./pages/about")
-  };
-
-}).call(this);
